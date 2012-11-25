376c165b8272a8cfaba0e82b33754c777d0f921b
Dominic Tarr <dominic.tarr@gmail.com>
2012-11-25 00:26:37 -0800

    use outer functions, not functions on binding

--- index.js
+++ index.js
@@ -44,9 +44,9 @@ module.exports = function () {
 
     //PRE HOOKS
 
-    var put = db._db.put
-    var del = db._db.del
-    var batch = db._db.batch
+    var put = db.put
+    var del = db.del
+    var batch = db.batch
 
     function callHooks (ch, opts, cb) {
       for (var i in prehooks) {
@@ -56,23 +56,23 @@ module.exports = function () {
       if(ch.length == 1) {
         var change = ch.shift()
         return change.type == 'put' 
-          ? put.call(db._db, change.key, change.value, opts, cb) 
-          : del.call(db._db, change.key, opts, cb)  
+          ? put.call(db, change.key, change.value, opts, cb) 
+          : del.call(db, change.key, opts, cb)  
       }
-      return batch.call(db._db, ch, opts, cb)
+      return batch.call(db, ch, opts, cb)
     }
 
-    db._db.put = function (key, value, opts, cb ) {
+    db.put = function (key, value, opts, cb ) {
       var batch = [{key: key, value: value, type: 'put'}]
       return callHooks(batch, opts, cb)
     }
 
-    db._db.del = function (key, opts, cb) {
+    db.del = function (key, opts, cb) {
       var batch = [{key: key, type: 'del'}]
       return callHooks(batch, opts, cb)
     }
 
-    db._db.batch = function (batch, opts, cb) {
+    db.batch = function (batch, opts, cb) {
       return callHooks(batch, opts, cb)
     }
   }
