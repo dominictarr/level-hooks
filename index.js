efbd403979320ae900299ee580aa061196f2db24
Dominic Tarr <dominic.tarr@gmail.com>
2013-02-28 07:34:15 +1300

    new api, planning for subdb

--- index.js
+++ index.js
@@ -2,30 +2,41 @@
 
 //this will be used by queue.
 
-module.exports = function () {
+module.exports = function (db) {
+  if(db) return hooks(db)
 
-  return function (db) {
+  function hooks (db) {
 
     if(db.hooks) {
-      //if(listener) db.hooks.post(listener)
       return     
     }
 
     var posthooks = []
     var prehooks  = []
 
+    function getPrefix (p) {
+      return p && (
+          'string' ===   typeof p        ? p
+        : 'string' ===   typeof p.prefix ? p.prefix
+        : 'function' === typeof p.prefix ? p.prefix()
+        :                                  ''
+        )
+    }
+
     db.hooks = {
-      post: function (posthook) {
-        db.on('hooks:post', posthook)
+      post: function (hook) {
+        db.on('hooks:post', hook)
         return db
       },
-      pre: function (hook) {
-        prehooks.push(hook)
+      pre: function (prefix, hook) {
+        if(!hook) hook = prefix, prefix = ''
+        prehooks.push({prefix: getPrefix(prefix), hook: hook})
         return db
       },
       posthooks: posthooks,
       prehooks: prehooks
     }
+
     //POST HOOKS
 
     function each (e) {
@@ -49,23 +60,34 @@ module.exports = function () {
     var del = db.del
     var batch = db.batch
 
-    function callHooks (isBatch, ch, opts, cb) {
-      for (var i in prehooks) {
-        ch = prehooks[i](ch)
-        if(!ch) return cb(new Error('vetoed'))
-      }
+    function callHooks (isBatch, b, opts, cb) {
 
-      ch = ch.filter(function (e) {
+      b.forEach(function hook(e, i) {
+        prehooks.forEach(function (h) {
+          if(e.key.indexOf(h.prefix) == 0)
+            h.hook(e, function (ch, db) {
+              if(ch === false)
+                return delete b[i]
+              var prefix = getPrefix(db) || h.prefix || ''
+              ch.key = prefix + ch.key
+              console.log('batch - add', ch)
+              b.push(ch)
+              hook(ch, b.length - 1)            
+            })
+        })
+      })
+
+      b = b.filter(function (e) {
         return e && e.type //filter out empty items
       })
 
-      if(ch.length == 1 && !isBatch) {
-        var change = ch.shift()
+      if(b.length == 1 && !isBatch) {
+        var change = b[0]
         return change.type == 'put' 
           ? put.call(db, change.key, change.value, opts, cb) 
           : del.call(db, change.key, opts, cb)  
       }
-      return batch.call(db, ch, opts, cb)
+      return batch.call(db, b, opts, cb)
     }
 
     db.put = function (key, value, opts, cb ) {
@@ -82,4 +104,6 @@ module.exports = function () {
       return callHooks(true, batch, opts, cb)
     }
   }
+
+  return hooks
 }
